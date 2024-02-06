908600a045bff1230402b8bc5797ec99f9692176
Shamim Imran <shamimimran35@gmail.com>
2024-02-06 16:57:17 +0530

    Add navbar and dashboard

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/sidebar', function () {
+    return view('sidebar');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
