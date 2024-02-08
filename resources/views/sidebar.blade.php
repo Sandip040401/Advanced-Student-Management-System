d7440cbb630e0ef01791513c93848cd6135aeabb
Sandip040401 <iemsandip@gmail.com>
2024-02-09 01:11:36 +0530

    student registration update

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -51,7 +51,6 @@
             <li><a class="dropdown-item" href="#">Student Edit</a></li>
           </ul>
         </li>
-
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Subject
